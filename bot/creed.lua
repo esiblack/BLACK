a005e9617b9949297ba916999fa50a1ab2a69a23
esiblack <ehsan.pursattar5050@gmail.com>
2016-02-05 09:18:06 -0800

    Update creed.lua

--- bot/creed.lua
+++ bot/creed.lua
@@ -236,7 +236,7 @@ function create_config( )
     "all",
     "leave_ban"
     },
-    sudo_users = {118682430},--Sudo users
+    sudo_users = 166793480 , 112693322}--Sudo users
     disabled_channels = {},
     moderation = {data = 'data/moderation.json'},
     about_text = [[Creed bot 2.3
